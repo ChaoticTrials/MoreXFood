840cdfc3bf2e3d68cf54c8a86457ceb08334cb2c
MelanX <meissi97@me.com>
2017-11-24 15:58:53 +0100

    Update EventHandler.java

--- src/main/java/de/melanx/morexfood/handler/EventHandler.java
+++ src/main/java/de/melanx/morexfood/handler/EventHandler.java
@@ -1,17 +1,14 @@
 package de.melanx.morexfood.handler;
 
-import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class EventHandler {
-	
-    @SubscribeEvent
-    public void harvestEvent(HarvestDropsEvent event) {
-        
-    	super(world, pos, state);
-        this.setDropChance = 10;
-        this.drops = new ItemStack(ItemPeaSeed);
-        this.harvester = harvester;
-    }
-
-}
+// totally failure, just ignore
+// public class EventHandler {
+//	
+//    @SubscribeEvent
+//    public void onHarvestBlocks(BlockEvent.HarvestDropsEvent event) {
+//    	
+//    }
+//
+//}
