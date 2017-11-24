5bbea540738ea40e07cc13f3221b1303f6beff99
MelanX <meissi97@me.com>
2017-11-24 21:00:15 +0100

    Added Carrot Pieces

---
+++ src/main/java/de/melanx/morexfood/item/ItemCarrotPieces.java
@@ -0,0 +1,24 @@
+package de.melanx.morexfood.item;
+
+import de.melanx.morexfood.morexfood;
+import net.minecraft.item.ItemFood;
+import net.minecraftforge.oredict.OreDictionary;
+
+public class ItemCarrotPieces extends ItemFood {
+	
+	public ItemCarrotPieces() {
+		super(1, 0.6f, false);
+		setUnlocalizedName("carrot_pieces");
+		setRegistryName("carrot_pieces");
+		setCreativeTab(morexfood.creativeTab);
+	}
+	
+	public void registerItemModel() {
+		morexfood.proxy.registerItemRenderer(this, 0, "carrot_pieces");
+	}
+	
+	public void initOreDict() {
+		OreDictionary.registerOre("piecesCarrot", this);
+	}
+
+}
