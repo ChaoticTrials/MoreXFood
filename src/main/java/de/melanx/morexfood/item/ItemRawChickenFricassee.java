2ffcfe67a4cd597cae75b73ce78ee3d9bd7b8db0
MelanX <meissi97@me.com>
2017-11-25 21:54:01 +0100

    added poison effect

--- src/main/java/de/melanx/morexfood/item/ItemRawChickenFricassee.java
+++ src/main/java/de/melanx/morexfood/item/ItemRawChickenFricassee.java
@@ -2,14 +2,19 @@ package de.melanx.morexfood.item;
 
 import de.melanx.morexfood.morexfood;
 import net.minecraft.item.ItemFood;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class ItemRawChickenFricassee extends ItemFood {
-		
+	
+	final Potion potion = Potion.getPotionFromResourceLocation("poison");
+	
 	public ItemRawChickenFricassee() {
 		super(4, 0.0f, false);
 		setUnlocalizedName("raw_chicken_fricassee");
 		setRegistryName("raw_chicken_fricassee");
 		setCreativeTab(morexfood.creativeTab);
+		setPotionEffect(new PotionEffect(potion, 150, 0), 95);
 	}
 		
 	public void registerItemModel() {
--- src/main/java/de/melanx/morexfood/item/ItemRawChickenFricasseeRice.java
+++ src/main/java/de/melanx/morexfood/item/ItemRawChickenFricasseeRice.java
@@ -2,14 +2,19 @@ package de.melanx.morexfood.item;
 
 import de.melanx.morexfood.morexfood;
 import net.minecraft.item.ItemFood;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class ItemRawChickenFricasseeRice extends ItemFood {
+	
+	final Potion potion = Potion.getPotionFromResourceLocation("poison");
 	
 	public ItemRawChickenFricasseeRice() {
 		super(5, 0.4f, false);
 		setUnlocalizedName("raw_chicken_fricassee_rice");
 		setRegistryName("raw_chicken_fricassee_rice");
 		setCreativeTab(morexfood.creativeTab);
+		setPotionEffect(new PotionEffect(potion, 100, 0), 90);
 	}
 	
 	public void registerItemModel() {
