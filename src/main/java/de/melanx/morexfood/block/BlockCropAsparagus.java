c01fceb4c34492546e8965d4a11e32f4187e2558
MelanX <meissi97@me.com>
2017-11-29 15:25:33 +0100

    Changed bounding box
    crops bounding box now better

--- src/main/java/de/melanx/morexfood/block/BlockCropAsparagus.java
+++ src/main/java/de/melanx/morexfood/block/BlockCropAsparagus.java
@@ -3,7 +3,10 @@ package de.melanx.morexfood.block;
 import de.melanx.morexfood.morexfood;
 import de.melanx.morexfood.items.ModItems;
 import net.minecraft.block.BlockCrops;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.EnumPlantType;
@@ -14,6 +17,25 @@ public class BlockCropAsparagus extends BlockCrops {
 		setUnlocalizedName("crop_asparagus");
 		setRegistryName("crop_asparagus");
 		setCreativeTab(morexfood.creativeTab);
+	}
+	
+	private static final int MATURE_AGE = 7;
+	
+	private static final PropertyInteger AGE = PropertyInteger.create("age", 0, MATURE_AGE);
+	
+	private static final AxisAlignedBB[] CROPS_AABB =
+			new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D),
+								new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D),
+										new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D),
+										new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D),
+										new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D),
+										new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D),
+										new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D),
+										new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D)};
+	
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return CROPS_AABB[state.getValue(AGE)];
 	}
 	
 	@Override
